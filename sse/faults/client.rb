53350544e76a17198051067a8ae64ce457cb8926
Luis Pureza <pureza@gmail.com>
2008-06-19 16:20:40 +0100

    Implemented (most of the) changes proposed by Duraes's state diagram.

--- sse/faults/client.rb
+++ sse/faults/client.rb
@@ -1,49 +1,60 @@
 #! /usr/bin/env ruby
 
 require 'socket'
+require 'logger'
 
 class FaultClient
-  
-  def initialize(host, port, injector_device, script)
-    @socket = TCPSocket.new(host, port)
-    @injector_device = injector_device
-    @script = script
-  end
 
-  def start
-    faults = nil
-    while true
-      message = @socket.gets.strip
-
-      case message
-      when /FAULT/
-        faults = message.split[1..-1].join.split("|")
-        puts "applying #{faults.length} faults..."
-        
-        File.open(@injector_device, "w") do |dev|
-          dev.write("+" + faults.join("\n+"))
-        end
-      when /EXECUTE/
-        puts "executing..."
-        result = `bash #{@script}`
-        @socket.puts(result)
-
-        if !faults.nil?
-          puts "removing #{faults.length} faults..."
-          File.open(@injector_device, "w") do |dev|
-            dev.write("-" + faults.join("\n+"))
-          end
-        end
-        faults = nil
-      end
+    def initialize(host, port, injector_device, script, logger)
+        @socket = TCPSocket.new(host, port)
+        @injector_device = injector_device
+        @script = script
+        @logger = logger
     end
-  end
+
+    def start
+        faults = nil
+        while true
+            message = @socket.gets.strip
+
+            case message
+            when /fault/
+                faults = message.split[1..-1].join.split("|")
+                @logger.info("received #{faults.length} faults")
+
+                File.open(@injector_device, "w") do |dev|
+                    dev.write("+" + faults.join("\n+"))
+                end
+
+                @logger.info("#{faults.length} faults injected")
+                @socket.puts("#{faults.length} faults injected")
+            when /execute/
+                @logger.info("started workload")
+                @socket.puts("started workload")
+
+                result = `bash #{@script}`
+
+                @logger.info("workload ended with result '#{result}'")
+                @socket.puts(result)
+
+                if !faults.nil?
+                    File.open(@injector_device, "w") do |dev|
+                        dev.write("-" + faults.join("\n+"))
+                    end
+
+                    @logger.info("#{faults.length} faults removed")
+                    @socket.puts("#{faults.length} faults removed")
+                end
+                faults = nil
+            end
+        end
+    end
 
 end
 
 if ARGV.length != 4
-  puts "Usage: client <server ip> <port> <injector device> <workload script>"
-  exit
+    puts "Usage: client <server ip> <port> <injector device> <workload script>"
+    exit
 end
 
 SERVER_IP = ARGV[0]
@@ -51,5 +62,10 @@ PORT = ARGV[1]
 INJECTOR_DEVICE = ARGV[2]
 SCRIPT = ARGV[3]
 
-client = FaultClient.new(SERVER_IP, PORT, INJECTOR_DEVICE, SCRIPT)
+# Create the logger
+logger = Logger.new("client.log")
+logger.datetime_format = "%a %Y/%m/%d %H:%M:%S"
+logger.info("client started #{'#' * 80}")
+
+client = FaultClient.new(SERVER_IP, PORT, INJECTOR_DEVICE, SCRIPT, logger)
 client.start
--- sse/faults/server.rb
+++ sse/faults/server.rb
@@ -18,142 +18,195 @@ TIMEOUT = 40
 FAULTS_PER_ROUND = 10
 
 class FaultServer
-  
-  def initialize(port, vms, faults, logger)
-    # The faults
-    @faults = faults
+    
+    def initialize(port, vms, faults, logger)
+        # The faults
+        @faults = faults
 
-    # Number of faults injected
-    @injected_faults = 0
+        # Number of faults injected
+        @injected_faults = 0
 
-    # The virtual machines
-    @vms = vms
+        # The virtual machines
+        @vms = vms
 
-    # This mutex synchronizes access to the VM's hashtable
-    @vms_mutex = Mutex.new
+        # This mutex synchronizes access to the VM's hashtable
+        @vms_mutex = Mutex.new
 
-    # VM's wait for each other before running their workload
-    @barrier = Barrier.new(@vms.length)
+        # VM's wait for each other before running their workload
+        @barrier = Barrier.new(@vms.length)
 
-    # The server's socket
-    @socket = TCPServer.new("", port)
-    @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, 1)
+        # The server's socket
+        @socket = TCPServer.new("", port)
+        @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, 1)
 
-    # Logger
-    @logger = logger
-  end
+        # Logger
+        @logger = logger
+    end
 
-  def start
-    while true
-      # Accept a new connection...
-      client_socket = @socket.accept
-           
-      # ... and handle it inside a new thread
-      Thread.new(client_socket) do |client|
+    def start
+        while true
+            # Accept a new connection...
+            client_socket = @socket.accept
+            
+            # ... and handle it inside a new thread
+            Thread.new(client_socket) do |client|
+                begin
+                    ip = client.peeraddr[3]
+                    @logger.info("#{ip} connected")
+                    handle_client(client)
+                rescue Exception => ex
+                    @logger.error("#{ip}: #{ex}\n\t from: " + ex.backtrace.join("\n\t from: "))
+                ensure
+                    begin 
+                        client.close 
+                    rescue 
+                    end
+                end
+            end
+        end
+    end
+
+    private
+
+    def handle_client(socket)
+        result = nil
+        ip = socket.peeraddr[3]
+        is_faulty = false
+        step = "Initializing..."
+
+        # Change the VM status to connected
+        @vms_mutex.synchronize {
+            throw "Unknown VM connection from '#{ip}'" unless @vms.has_key?(ip)
+
+            if @vms[ip].status == :disconnected
+                @vms[ip].status = :connected
+            else
+                throw "The VM '#{@vms[ip].hostname}' is already connected!"
+            end
+
+            # While we have the lock, let's read some VM properties to use later
+            is_faulty = @vms[ip].faulty?
+        }
+
         begin
-          ip = client.peeraddr[3]
-          @logger.info("CONNECTED\t\t #{ip}")
-          handle_client(client)
+            # Keep making requests until some error occurs
+            while @injected_faults < @faults.length
+
+                # Send the faults to the faulty VM
+                if is_faulty
+                    step = "Sending faults..."
+
+                    to_inject = @faults[@injected_faults .. (@injected_faults + FAULTS_PER_ROUND - 1)] # 3
+                    socket.puts("fault #{to_inject.join("|")}")                      # 4
+                    @logger.info("#{ip} faults sent (#{@injected_faults}.." \
+                                 "#{@injected_faults + FAULTS_PER_ROUND - 1})")
+                                # "\n\t#{to_inject.join("\n\t")}") # 5
+                    @injected_faults += to_inject.length
+
+                    step = "Faults sent. Waiting for injection confirmation..."
+
+                    Timeout::timeout(TIMEOUT) do
+                        injected = socket.gets # 6
+
+                        if injected =~ /(\d+) faults injected/ && $1.to_i == FAULTS_PER_ROUND
+                            @logger.info("#{ip} #{$1} faults injected") # 7
+                        else
+                            throw "Unexpected value read from socket. Expected " \
+                            "was 'faults injected', but received '#{injected}'"
+                        end
+                    end
+                end
+
+                step = "Waiting for the other VMs..."
+
+                # Wait until all VM's are ready
+                @barrier.wait
+
+                step = "Starting the workload..."
+
+                @logger.info("#{ip} starting workload")
+
+                # Make the request
+                socket.puts("execute")
+                time_execute_sent = Time.now
+
+                step = "Workload started! Receiving confirmation from the VM..."
+
+                # Read the result and compare it with the correct one
+                Timeout::timeout(is_faulty ? TIMEOUT : 0) do
+                    started = socket.gets
+                    if started =~ /started workload/
+                        @logger.info("#{ip} started workload")
+                    else
+                        throw "Unexpected value read from socket. Expected " \
+                        "was 'started workload', but received '#{started}'"
+                    end
+
+                    step = "Waiting for the workload result..."
+
+                    result = socket.gets
+                    throw "socket read nil result" if result.nil?
+
+                    result = result.strip
+                    throw "wrong result" if result != CORRECT_RESULT
+
+                    @logger.info("#{ip} correct (#{sprintf("%.0f", Time.now - time_execute_sent)}s)")
+                end
+
+                step = "Waiting for fault removal confirmation..."
+
+                # Wait until the faulty machine removes the previous faults
+                if is_faulty
+                    Timeout::timeout(TIMEOUT) do
+                        removed = socket.gets
+
+                        if removed =~ /(\d+) faults removed/ && $1.to_i == FAULTS_PER_ROUND
+                            @logger.info("#{ip} #{$1} faults removed")
+                        else
+                            throw "Unexpected value read from socket. Expected " \
+                            "was 'faults removed', but received '#{removed}'"
+                        end
+                    end
+                end
+            end
+
+            @logger.info("Injection complete.")
+        rescue Timeout::Error
+            # receive timedout
+            @logger.error("#{ip} timeout on: '#{step}'")
+            reboot_vm(ip)
         rescue Exception => ex
-          @logger.error("#{ip}: #{ex}\n\t from: " + ex.backtrace.join("\n\t from: "))
-        ensure
-          begin 
-            client.close 
-          rescue 
-          end
+            if !result.nil? && result != CORRECT_RESULT
+                # incorrect result
+                @logger.error("#{ip} wrong: '#{result}'")
+            else
+                # Some other kind of error ocurred... one which was not supposed to happen :-S
+                @logger.error("#{ip}: #{ex}\n\t from: " + ex.backtrace.join("\n\t from: "))
+            end
+            reboot_vm(ip)
         end
-      end
     end
-  end
 
-  private
+    def reboot_vm(ip)
+        @vms_mutex.synchronize {
+            output = @vms[ip].reboot()
+            @logger.info("#{ip} reboot #{log_status}")
+            @logger.error("#{ip} vmware-cmd error: #{output}") unless output == ""
+            @logger.fatal("#{ip} is not faulty!") if !@vms[ip].faulty?
+        }
+    end
 
-  def handle_client(socket)
-    result = ""
-    ip = socket.peeraddr[3]
-    is_faulty = false
-
-    # Change the VM status to connected
-    @vms_mutex.synchronize {
-      throw "Unknown VM connection from '#{ip}'" unless @vms.has_key?(ip)
-
-      if @vms[ip].status == :disconnected
-        @vms[ip].status = :connected
-      else
-        throw "The VM '#{@vms[ip].hostname}' is already connected!"
-      end
-
-      # While we have the lock, let's read some VM properties to use later
-      is_faulty = @vms[ip].faulty?
-    }
- 
-    begin
-      # Keep making requests until some error occurs
-      while @injected_faults < @faults.length
-
-        # Send the faults to the faulty VM
-        if is_faulty
-          to_inject = @faults[@injected_faults .. (@injected_faults + FAULTS_PER_ROUND - 1)]
-          socket.puts("FAULT #{to_inject.join("|")}")
-          @logger.info("INJECTION\t\t #{ip} ##{@injected_faults + 1}\n\t#{to_inject.join("\n\t")}")
-          @injected_faults += to_inject.length
+    def log_status
+        status = "\n------------------------------------------------\n"
+        status +=  "Hostname         | Status           | # reboots   \n"
+        status +=  "-----------------+------------------+-----------\n"
+        for value in @vms.values
+            status += value.to_s + "\n"
         end
-
-        # Wait until all VM's are ready
-        @barrier.wait
-
-        # Make the request
-        socket.puts("EXECUTE")
-        time_execute_sent = Time.now
-
-        # Read the result and compare it with the correct one
-        Timeout::timeout(is_faulty ? TIMEOUT : 0) do
-          result = socket.gets
-          throw "socket read nil result" if result.nil?
-
-          result = result.strip
-          throw "wrong result" if result != CORRECT_RESULT
-
-          @logger.info("DURATION:\t\t #{ip} #{sprintf("%.0f", Time.now - time_execute_sent)}s")
-        end
-      end
-      @logger.info("Injection complete.")
-    rescue Timeout::Error
-      # receive timedout
-      @logger.error("TIMEOUT\t\t #{ip}")
-      reboot_vm(ip)
-    rescue Exception => ex
-      if !result.nil? && result != CORRECT_RESULT
-        # incorrect result
-        @logger.error("WRONG\t\t #{ip} #{result}")
-      else
-        # Some other kind of error ocurred... one which was not supposed to happen :-S
-        @logger.error("#{ip}: #{ex}\n\t from: " + ex.backtrace.join("\n\t from: "))
-      end
-      reboot_vm(ip)
+        status +=  "------------------------------------------------\n"
+        status += "#{@injected_faults}/#{@faults.length} faults injected"
+        status
     end
-  end
-
-  def reboot_vm(ip)
-    @vms_mutex.synchronize {
-      output = @vms[ip].reboot()
-      @logger.info("RESTART\t\t #{ip}#{log_status}")
-      @logger.error("VMWARE-CMD ERROR\t #{ip}: #{output}") unless output == ""
-      @logger.fatal("#{ip} is not faulty!") if !@vms[ip].faulty?
-    }
-  end
-
-  def log_status
-    status = "\n------------------------------------------------\n"
-    status +=  "Hostname         | Status           | # reboots   \n"
-    status +=  "-----------------+------------------+-----------\n"
-    for value in @vms.values
-      status += value.to_s + "\n"
-    end
-    status +=  "------------------------------------------------\n"
-    status += "#{@injected_faults}/#{@faults.length} faults injected"
-    status
-  end
 end
 
 
@@ -162,18 +215,18 @@ end
 #
 def load_configuration(file)
     File.open(file) do |fx|
-      faults_file, port, vms_config = YAML::load_stream(fx).documents
+        faults_file, port, vms_config = YAML::load_stream(fx).documents
 
-      vms = {}
-      vms_config.each { |k, v| vms[k] = VirtualMachine.new(v.hostname, v.vmx, v.product_server, v.product_username, v.faulty?) }
+        vms = {}
+        vms_config.each { |k, v| vms[k] = VirtualMachine.new(v.hostname, v.vmx, v.product_server, v.product_username, v.faulty?) }
 
-      return [faults_file, port, vms]
+        return [faults_file, port, vms]
     end
 end
 
 if ARGV.length != 1
-  puts "Usage: server <configuration>"
-  exit
+    puts "Usage: server <configuration>"
+    exit
 end
 
 # load virtual machine settings
@@ -181,13 +234,13 @@ FAULTS_FILE, PORT, VMS = load_configuration(ARGV[0])
 
 # read faults
 faults = IO.readlines(FAULTS_FILE).map do |fault|
-  fault.strip
+    fault.strip
 end
 
 # Create the logger
 logger = Logger.new("#{ARGV[0].split(".").first}.log")
 logger.datetime_format = "%a %Y/%m/%d %H:%M:%S"
-logger.info("SESSION START")
+logger.info("server started #{'#' * 80}")
 
 server = FaultServer.new(PORT, VMS, faults, logger)
 server.start
